f12091749be30b814626e3a0862301583cf74f69
soo-utem <soo@uteme.edu.my>
2023-10-10 13:13:58 +0800

    fixed auto start

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://soo:wgDGNihzILPCiMbr@cluster0.j77yadh.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      //  await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      //  await client.close();
+   }
+}
+run().catch(console.dir);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+   res.sendFile('register.html', { root: __dirname })
+})
+
+app.post('/register', async (req, res) => {
+   console.log('registration', req.body)
+
+   return await client.db('maybank2u').collection('users').insertOne(req.body)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+   console.log(`Example app listening on port ${port}`)
 })
